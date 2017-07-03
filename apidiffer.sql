5da21f2ce3aef00be09f2b69b6aa52119080ecc9
hanxueming <hanxmc@yonyou.com>
2017-07-03 15:44:27 +0800

    add sql

---
+++ apidiffer.sql
@@ -0,0 +1,53 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.6.23)
+# Database: apidiffer
+# Generation Time: 2017-07-03 07:44:05 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table env_config
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `env_config`;
+
+CREATE TABLE `env_config` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `master_protocol` varchar(128) NOT NULL,
+  `master_host` varchar(128) NOT NULL DEFAULT '',
+  `master_ip` varchar(20) NOT NULL,
+  `master_port` int(5) NOT NULL DEFAULT '80',
+  `master_token` varchar(128) NOT NULL DEFAULT '',
+  `test_protocol` varchar(128) NOT NULL,
+  `test_host` varchar(128) NOT NULL DEFAULT '',
+  `test_ip` varchar(20) NOT NULL DEFAULT '',
+  `test_port` int(5) NOT NULL DEFAULT '80',
+  `test_token` varchar(128) NOT NULL DEFAULT '',
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
